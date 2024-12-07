d6801e8e77cdf7fa45e4056425cfa09f8927bfd7
Mashf1q <mashfiq257@gmail.com>
2024-12-07 22:04:09 +0600

    Add files via upload

---
+++ C/areacalculator.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<math.h>
+
+float area_circle();
+float area_rectangle();
+float area_square();
+
+float area;
+
+
+int main() {
+int choice;
+
+//taking area 
+printf("enter shape : \n1. circle\n2. rectangle\n3. square\n");
+scanf("%d",&choice);
+
+//calling fuc
+if(choice == 1)  area_circle();
+if(choice == 2)  area_rectangle();
+if(choice == 3)  area_square();
+
+printf("area ;%f",area);
+return 0;
+}
+
+float area_circle(){
+float calc1;
+float radius;
+
+printf("enter radius :");
+scanf("%f",&radius);
+
+calc1 = radius * radius;
+area = calc1 * 3.1416;
+}
+
+float area_rectangle(){
+float len;
+float hig;
+
+printf("enter lenght  :");
+scanf("%f",&len);
+
+printf("enter hight :");
+scanf("%f",&hig);
+
+area = len * hig; 
+}
+
+float area_square(){
+
+float len;
+
+printf("enter lenght");
+scanf("%f",&len);
+
+area = len * len;
+}
+
